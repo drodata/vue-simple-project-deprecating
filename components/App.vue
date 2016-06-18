bc1fec841939cbf8a2f986220ed8a76d3a08056d
drodata <drodata@foxmail.com>
2016-06-18 10:52:04 +0800

    add another simple component AppFooter

--- components/App.vue
+++ components/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="app">
     <component-a></component-a>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import ComponentA from './ComponentA.vue'
+import AppFooter from './AppFooter.vue'
 
 export default {
   components: {
-    ComponentA
+    ComponentA,
+    AppFooter
   }
 }
 </script>
---
+++ components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'This is footer area.'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
+
